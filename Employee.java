bec46bbdf3ab1c2ce7da98540c9702cb12f64c8a
Abhishek-20044 <abhishekpattar00@gmail.com>
2025-05-07 16:03:56 +0530

    second commit

--- Employee.java
+++ Employee.java
@@ -2,27 +2,26 @@ public class Employee {
     String name;
     int id;
     double salary;
-    int phonenum;
-    String address;
 
-    public Employee(String name, int id, double salary) {
+    
+
+    public Employee(String name, int id, double salary ) {
         this.name = name;
         this.id = id;
         this.salary = salary;
-        this.phonenum= phonenum;
-        this.address= address;
+   ;
     }
 
     public void displayInfo() {
         System.out.println("Employee ID: " + id);
         System.out.println("Employee Name: " + name);
         System.out.println("Employee Salary: " + salary);
-        System.out.println("Employee phonenum: "+phonenum);
-        System.out.println("Employee Adress: "+address);
+      
+       
     }
 
     public static void main(String[] args) {
-        Employee emp1 = new Employee("xyz", 101, 50000.0, 12345678798, hubli);
+        Employee emp1 = new Employee("xyz", 101, 50000.0);
         emp1.displayInfo();
     }
 }
