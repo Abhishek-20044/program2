ac348d71324924c4dd92fce9de6da58e992d25ed
Abhishek-20044 <abhishekpattar00@gmail.com>
2025-05-07 16:09:42 +0530

    Initial commit

--- Employee.java
+++ Employee.java
@@ -2,13 +2,15 @@ public class Employee {
     String name;
     int id;
     double salary;
+    String address;
 
     
 
-    public Employee(String name, int id, double salary ) {
+    public Employee(String name, int id, double salary,String address ) {
         this.name = name;
         this.id = id;
         this.salary = salary;
+        this.address=address;
    ;
     }
 
@@ -17,11 +19,11 @@ public class Employee {
         System.out.println("Employee Name: " + name);
         System.out.println("Employee Salary: " + salary);
       
-       
+        System.out.println("Employee address: " + address);
     }
 
     public static void main(String[] args) {
-        Employee emp1 = new Employee("xyz", 101, 50000.0);
+        Employee emp1 = new Employee("xyz", 101, 50000.0,"hubli");
         emp1.displayInfo();
     }
 }
